73ff379dcec0302a397bf68a9649ec1da4e6b059
Carey H <blujedicorp@gmail.com>
2020-11-26 00:51:20 -0800

    add enable feature

--- src/addons/cli.js
+++ src/addons/cli.js
@@ -2,6 +2,8 @@ const argv = process.argv.slice(2);
 const cmd = argv[0];
 const { redBright } = require('ansi-colors');
 const runner = require('./runner');
+const pkg = require('../../package.json');
+const { writeJSONSync } = require('fs-extra');
 
 let action = argv.includes('--remove') || argv.includes('-r') ? 'remove' : 'add';
 
@@ -9,6 +11,9 @@ const { run, config } = runner(cmd, action);
 
 if (cmd && config) {
   run();
+  pkg.addons = pkg.addons || [];
+  pkg.addons.push(cmd);
+  writeJSONSync('../../package.json', pkg);
 }
 else {
   console.log(redBright(`\nSorry have no clue what the "${cmd}" addon is!\n`));
