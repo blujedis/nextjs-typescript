25dddf373a4748dba96f360c6132f2e8903614a0
Carey H <blujedicorp@gmail.com>
2020-11-26 09:46:08 -0800

    fix cli args

--- src/addons/runner.js
+++ src/addons/runner.js
@@ -3,7 +3,7 @@ const { spawnSync } = require('child_process');
 const { magentaBright } = require('ansi-colors');
 
 const packages = {
-  antd: { dependencies: ['antd', 'less'], devDependencies: ['@zeit/next-less', '@zeit/next-css', 'next-compose-plugins', 'less-js-to-vars'] },
+  antd: { dependencies: ['antd', 'less'], devDependencies: ['@zeit/next-less', '@zeit/next-css', 'next-compose-plugins', 'less-vars-to-js'] },
   bulma: { dependencies: ['bulma'], devDependencies: ['node-sass@4.14.1'] },
   firebase: { dependencies: ['firebase', 'firebase-admin', 'js-cookie', 'react-firebaseui'], devDependencies: ['@types/js-cookie'] },
   nextauth: { dependencies: ['next-auth'], devDependencies: ['@types/next-auth'] },
@@ -25,8 +25,11 @@ module.exports = (cmd, action) => {
       ...options
     };
 
+    args = args.flat();
+
     if (dev)
       args.push('-D');
+
 
     console.log(`${magentaBright('addon')} running command "${runCmd + ' ' + args[0]} ${args.slice(1).join(' ')}"`);
     spawnSync(runCmd, args, options);
